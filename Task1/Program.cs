15c6aec1d3eebb4a5f44da3df3723a33beafda25
lavetos <Hleb_Lavetka@epam.com>
2023-11-12 18:21:51 +0300

    Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            PrintFirstCharacter();
+        }
+
+        private static void PrintFirstCharacter()
+        {
+            try
+            {
+                Console.WriteLine("Write some Text");
+                var enteredText = Console.ReadLine();
+
+                if (enteredText == null)
+                {
+                    throw new System.ArgumentNullException("Text is not written");
+                }
+                if (enteredText.Trim() == "")
+                {
+                    throw new System.ArgumentException("Characters were not added");
+                }
+                Console.WriteLine(enteredText[0]);
+            }
+            catch (ArgumentNullException a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            catch (ArgumentException a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected Error appears{e}");
+            }
         }
     }
 }
